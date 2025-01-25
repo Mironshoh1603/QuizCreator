590e07554278a412b39cb54f321f7f019a2a9352
Mironshoh <mironshohasadov1603@gmail.com>
2025-01-25 10:53:21 +0500

    add dele listenter

--- script.js
+++ script.js
@@ -33,14 +33,15 @@ let renderData = (data) => {
       row.append(varian);
     });
     let editTd = document.createElement("td");
-
+    let ind =
+      tests.indexOf(test) == -1 ? tests.length - 1 : tests.indexOf(test);
     let deleteTd = document.createElement("td");
     let editBtn = document.createElement("i");
     editBtn.setAttribute("class", "fa-solid fa-pen-to-square");
-    editBtn.setAttribute("id", data.indexOf(test));
+    editBtn.setAttribute("id", ind);
     let deleteBtn = document.createElement("i");
     deleteBtn.setAttribute("class", "fa-solid fa-trash");
-    deleteBtn.setAttribute("id", data.indexOf(test));
+    deleteBtn.setAttribute("id", ind);
 
     editTd.append(editBtn);
     deleteTd.append(deleteBtn);
@@ -85,5 +86,32 @@ btn.addEventListener("click", (event) => {
     document.querySelector("form").reset();
   } catch (error) {
     alert(error.message);
+  }
+});
+
+let deleteFunc = (event, value) => {
+  console.log("nima", event, value);
+};
+
+// let deleteBtns = document.querySelectorAll(".fa-trash");
+// [...deleteBtns].map((dalele) => {
+//   dalele.addEventListener("click", (event) => {
+//     console.log(event.target);
+//   });
+// });
+
+document.querySelector("table").addEventListener("click", (event) => {
+  let element = event.target;
+  console.log();
+  if ([...event.target.classList].includes("fa-trash")) {
+    let table = document.querySelector("table");
+    // console.log(table.children);
+    // console.log(+event.target.id + 1);
+    for (let index = tests.length - 1; index >= 0; index--) {
+      console.log(index);
+      table.deleteRow(index + 1);
+    }
+    tests.splice(element.id, 1);
+    renderData(tests);
   }
 });
